e2825a1734b292a6755be47a0b5d5ad8f0db08c6
EpicToastTM <epictoast1868@gmail.com>
2019-05-14 18:58:39 -0700

    Brown is now brown. Add more logging.

--- Assets/BrushStrokesScript.cs
+++ Assets/BrushStrokesScript.cs
@@ -26,6 +26,7 @@ public class BrushStrokesScript : MonoBehaviour
     private readonly static char[] literallyJustTheEntireAlphabet = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
     private readonly static char[] andAlsoSomeNumbers = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
     private readonly static string vowels = "AEIOU";
+    private readonly static string[] colorNames = { "red", "orange", "yellow", "lime", "green", "cyan", "sky", "blue", "purple", "magenta", "brown", "white", "gray", "black", "pink" };
     private int keyNum = 0;
 
     private bool[,] gaps = { { false, false, false, false, false, false },  // horizontal lines
@@ -310,6 +311,8 @@ public class BrushStrokesScript : MonoBehaviour
             colors[i] = Random.Range(0, 15);
             btnRenderers[i].material = btnColors[colors[i]];
         }
+
+        DebugMsg("The middle color is " + colorNames[colors[4]] + ".");
 
         if (colors[4] == 0) // if center point is red...
         {
