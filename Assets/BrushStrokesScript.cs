133c62cdecafc7442e42458d0be91232b6fff512
TasThing <lufarionite@gmail.com>
2020-06-29 12:43:05 -0400

    Fix a pattern

--- Assets/BrushStrokesScript.cs
+++ Assets/BrushStrokesScript.cs
@@ -213,7 +213,7 @@ public class BrushStrokesScript : MonoBehaviour
         {
             { true, true, false, true, true, false },
             { true, false, false, true, true, true },
-            { false, false, false, false, false, false },
+            { true, false, false, true, false, false },
             { false, false, false, false, false, false }
         }, // 30th symbol
         {
