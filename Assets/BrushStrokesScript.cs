e4dc38d64172798ddc3de382f2a2eef7f89748ea
tasthing <lufarionite@gmail.com>
2021-05-10 23:40:29 -0400

    bug fix

--- Assets/BrushStrokesScript.cs
+++ Assets/BrushStrokesScript.cs
@@ -211,8 +211,8 @@ public class BrushStrokesScript : MonoBehaviour
         }, // 29th symbol
         {
             { true, true, false, true, true, false },
-            { true, false, true, true, false, true },
-            { true, false, false, true, false, false },
+            { true, false, false, true, true, true },
+            { false, false, false, false, false, false },
             { false, false, false, false, false, false }
         }, // 30th symbol
         {
